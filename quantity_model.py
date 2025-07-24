ca50787a7dbf7991c28195d62d2986752565ef05
Zhang-XC <zhang_xiaocheng@outlook.com>
2025-07-24 22:50:29 +0200

    Move input parameters of quantity model to JSON config, update input data

--- quantity_model.py
+++ quantity_model.py
@@ -1,10 +1,23 @@
-import argparse
+import os
+import json
 
 import numpy as np
 import pandas as pd
 
 
-# %% Kinetic parameters (Palmer et al., 2021)
+# Paths
+INPUT_FILE = os.path.join("data", "quantity_model", "input.csv")
+OUTPUT_PATH = os.path.join("results", "quantity_model")
+OUTPUT_FILE = os.path.join(OUTPUT_PATH, "output.csv")
+
+if not os.path.exists(OUTPUT_PATH):
+    os.makedirs(OUTPUT_PATH)
+
+with open("config.json", "r") as f:
+    config = json.load(f)
+config = config["quantity_model"]
+
+# Kinetic parameters (Palmer et al., 2021)
 Af1 = 5.73 * 10 ** 5
 Af2 = 1.73 * 10 ** 6
 Af3 = 5.03 * 10 ** 5
@@ -14,40 +27,17 @@ Ed = 44.56
 K_Cl = 0.0509
 K_metal = 0.00259
 
-# %% Input parameters
-parser = argparse.ArgumentParser()
-parser.add_argument("--input_file", "-i", default="./data/input_quantity_model.csv", type=str,
-                    help="Path to CSV file of input data")
-parser.add_argument("--output_file", "-o", default="./data/output_quantity_model.csv", type=str,
-                    help="Path to CSV file of results")
-parser.add_argument("--T_comb", "-tc", default=950, type=float,
-                    help="Combustion temperature [°C]")
-parser.add_argument("--t_res", "-tr", default=2, type=float,
-                    help="Residence time [s]")
-parser.add_argument("--hours", "-hr", default=8050, type=int,
-                    help="Annual hours of operation [h]")
-parser.add_argument("--f_ocdf", "-f", default=0.207, type=float,
-                    help="Fraction of OCDF in total PCDD/Fs [-]")
-parser.add_argument("--R_esp", "-re", default=-1.61, type=float,
-                    help="Removal efficiency of ESP [-]")
-parser.add_argument("--R_ws", "-rw", default=0.4, type=float,
-                    help="Removal efficiency of WS [-]")
-parser.add_argument("--year_ws", "-y", default=1982, type=int,
-                    help="Year of WS installation")
-args = parser.parse_args()
-
-data = pd.read_csv(args.input_file, skiprows=1, index_col="Year")
+# Input parameters
+data = pd.read_csv(INPUT_FILE, skiprows=1, index_col="year")
 data["f_metal"] = data["f_Fe"] + data["f_Cu"]
 
-T_comb = args.T_comb + 273.15
-t_res = args.t_res
-hours = args.hours
-f_OCDF = args.f_ocdf
-R_esp = args.R_esp
-R_ws = args.R_ws
-year_ws = args.year_ws
+T_comb = config["combustion_temperature"]
+t_res = config["residence_time"]
+hours = config["yearly_operation_hours"]
+f_OCDF = config["ocdf_fraction"]
+rem_effs = config["removal_efficiencies"]
 
-# %% Calculate kinetic parameters
+# Calculate kinetic parameters
 R = 8.314 * 10 ** (-3) # gas constant, kJ/(mol·K)
 ratio_O = (data["m_air"] * 23 + 100 * data["f_O"] * data["m_waste"]) \
     / (data["m_waste"] * 100 * (data["f_C"] / 12 + data["f_H"] / 4 + data["f_S"] / 32) * 32) # oxygen ratio
@@ -58,19 +48,16 @@ kf2 = Af2 * np.exp(-Ef / (R * T_comb)) # formation kinetics constant for OCDF, (
 kf3 = Af3 * np.exp(-Ef / (R * T_comb)) # formation kinetics constant for 1,2,3,6,7,8-HxCDD, (pg PCDD/F)/(g waste·s)
 kd = Ad * np.exp(-Ed / (R * T_comb)) # decomposition kinetics constant, (pg PCDD/F)/(g waste·s)
 
-# %% Solve kinetic equation
+# Solve kinetic equation
 emission_rate = kf2 * E_Cl * E_metal / kd * (1 - np.exp(-kd * ratio_O * t_res))
 
-# %% Post-processing
-results = pd.DataFrame({
-    "Year": data.index,
-    "Stack emission [g]": [pd.NA] * len(data),
-}).set_index("Year")
+# Post-processing
+results = pd.DataFrame(index=data.index, columns=["stack_emission"])
+results.index.name = "year"
 
-emission_yearly = emission_rate * data["m_waste"] * hours * 3600 * 10 ** (-9) / f_OCDF
-results["Stack emission [g]"] = emission_yearly * (1 - R_esp)
-results.loc[:year_ws, "Stack emission [g]"] = emission_yearly * (1 - R_esp)
-results.loc[year_ws:, "Stack emission [g]"] = emission_yearly * (1 - R_esp) * (1 - R_ws)
+results["stack_emission"] = emission_rate * data["m_waste"] * hours * 3600 * 10 ** (-9) / f_OCDF
+for rem_eff in rem_effs:
+    results["stack_emission"] = results["stack_emission"] * (1 - rem_eff)
 
-# %% Save results
-results.to_csv(args.output_file, index_label="Year", index=True)
+# Save results
+results.to_csv(OUTPUT_FILE, index_label="year", index=True)
